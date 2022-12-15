dcceca883d473df4bd9fd7da7c3e88a4eedb90d5
HenningCV <henningvenhorst@gmail.com>
2022-12-15 15:22:12 +0100

    finding the closest enemy in range functionalities add

---
+++ Assets/Scripts/Tower/Tower.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    // the current target of the tower
+    public Transform target;
+    // the maximum range of the turret
+    public float range = 15f;
+    // the tag to find the enemies
+    public string enemyTag = "Enemy";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    // finding the closest enemy in range
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+        foreach(GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if(distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if(nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target == null){
+            return;
+        }
+    }
+
+    // drawing a red gizmo around the selected tower that indicates the towers range
+    private void OnDrawGizmosSelected() 
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
