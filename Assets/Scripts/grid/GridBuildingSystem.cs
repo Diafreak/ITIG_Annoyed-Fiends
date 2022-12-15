7b865b8187da43cc5f2a820a130b26e0584074d9
Diafreak <diafreak212@gmail.com>
2022-12-16 00:52:54 +0100

    Moved GridObject in separate File, Fixed NullPtrExeption on RightMouseClick

--- Assets/Scripts/grid/GridBuildingSystem.cs
+++ Assets/Scripts/grid/GridBuildingSystem.cs
@@ -32,47 +32,6 @@ public class GridBuildingSystem : MonoBehaviour
     }
 
 
-    // Object-definition of the object that is placed on the Grid
-    public class GridObject {
-
-        // Reference to the grid | contains a GridObject on each grid-position
-        private GridXZ<GridObject> grid;
-        private int x;
-        private int z;
-        private PlacedTower placedTower;
-
-        // constructor
-        public GridObject(GridXZ<GridObject> grid, int x, int z) {
-            this.grid = grid;
-            this.x = x;
-            this.z = z;
-        }
-
-
-        public void SetPlacedTower(PlacedTower placedTower) {
-            this.placedTower = placedTower;
-            grid.TriggerGridObjectChanged(x, z);
-        }
-
-        public void ClearPlacedTower() {
-            this.placedTower = null;
-            grid.TriggerGridObjectChanged(x, z);
-        }
-
-        public PlacedTower GetPlacedTower() {
-            return this.placedTower;
-        }
-
-        public bool CanBuild() {
-            return placedTower == null;
-        }
-
-        public override string ToString()
-        {
-            return x + ", " + z + "\n" + placedTower;
-        }
-    }
-
 
     private void Update() {
         // Build Tower
@@ -100,11 +59,15 @@ public class GridBuildingSystem : MonoBehaviour
         if (Input.GetMouseButtonDown(1)) {
             // Get clicked Grid-Tile
             GridObject gridObject = grid.GetGridObject(GridUtils.GetMouseWorldPosition3d(mouseColliderLayerMask));
-            // Get the Tower on that Tile
-            PlacedTower placedTower = gridObject.GetPlacedTower();
-            if (placedTower != null ) {
-                placedTower.DestroySelf();
-                gridObject.ClearPlacedTower();
+
+            if (gridObject != null) {
+                // Get the Tower on that Tile
+                PlacedTower placedTower = gridObject.GetPlacedTower();
+
+                if (placedTower != null ) {
+                    placedTower.DestroySelf();
+                    gridObject.ClearPlacedTower();
+                }
             }
         }
 
---
+++ Assets/Scripts/grid/GridObject.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Object-definition of the object that is placed on the Grid
+public class GridObject
+{
+    // Reference to the grid | contains a GridObject on each grid-position
+    private GridXZ<GridObject> grid;
+    private int x;
+    private int z;
+    private PlacedTower placedTower;
+
+    // constructor
+    public GridObject(GridXZ<GridObject> grid, int x, int z) {
+        this.grid = grid;
+        this.x = x;
+        this.z = z;
+    }
+
+
+    public void SetPlacedTower(PlacedTower placedTower) {
+        this.placedTower = placedTower;
+        grid.TriggerGridObjectChanged(x, z);
+    }
+
+    public void ClearPlacedTower() {
+        this.placedTower = null;
+        grid.TriggerGridObjectChanged(x, z);
+    }
+
+    public PlacedTower GetPlacedTower() {
+        return this.placedTower;
+    }
+
+    public bool CanBuild() {
+        return placedTower == null;
+    }
+
+    public override string ToString()
+    {
+        return x + ", " + z + "\n" + placedTower;
+    }
+}
