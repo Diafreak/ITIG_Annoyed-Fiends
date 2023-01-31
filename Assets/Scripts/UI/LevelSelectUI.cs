8190f81f265e2e74b04ecf33a71b14e6942ed5d5
Diafreak <diafreak212@gmail.com>
2023-01-31 19:01:38 +0100

    added level select images

--- Assets/Scripts/UI/LevelSelectUI.cs
+++ Assets/Scripts/UI/LevelSelectUI.cs
@@ -1,24 +1,40 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
+
 
 public class LevelSelectUI : MonoBehaviour {
 
+    [Header("Colors")]
+    public Color activatedTextColor;
+    public Color deactivatedTextColor;
+
+    [Header("Level Select Buttons")]
     public Button[] levelButtons;
+
+    [Header("Reset Player Prefs")]
     public int levelForPlayerPrefs = 0;
 
 
     private void Start() {
+        // just for Resetting the PlayerPrefs
         if (levelForPlayerPrefs != 0) {
             PlayerPrefs.SetInt("levelsUnlocked", levelForPlayerPrefs);
         }
 
-        int levelsUnlocked = PlayerPrefs.GetInt("levelsUnlocked", 1);
+        int levelsUnlocked = PlayerPrefs.GetInt("levelsUnlocked", 1);  // 1 = Default Value when "levelsUnlocked" is not yet set
 
-        for (int i = levelsUnlocked; i < levelButtons.Length; i++) {
-            levelButtons[i].interactable = false;
+        for (int i = 0; i < levelButtons.Length; i++) {
+            if (i < levelsUnlocked) {
+                levelButtons[i].transform.GetChild(0).GetComponent<TMP_Text>().color = activatedTextColor;
+            } else {
+                levelButtons[i].interactable = false;
+                levelButtons[i].transform.GetChild(0).GetComponent<TMP_Text>().color = deactivatedTextColor;
+            }
         }
     }
+
 
     public void SelectLevel(string levelName) {
         SceneManager.LoadScene(levelName);
