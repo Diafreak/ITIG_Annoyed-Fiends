b9d1d06bba42fcccdcd546bdf6a7b291ad306ac0
Diafreak <diafreak212@gmail.com>
2023-02-04 19:44:52 +0100

    removed visual-field

--- Assets/Scripts/ScriptableObjects/GridTileSO.cs
+++ Assets/Scripts/ScriptableObjects/GridTileSO.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "GridTileSO", menuName = "ScriptableObjects/GridTileSO")]
 public class GridTileSO : ScriptableObject {
 
+    [Header("Prefab")]
     public Transform visual;
+
+    [Header("Colors")]
     public Color defaultColor;
     public Color highlightColor;
     public Color insufficientMoneyColor;
